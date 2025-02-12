f2900d8be8e54f91f1564f3b05a3621273690624
Edder Cid <yeyo.foster@gmail.com>
2025-02-12 02:09:38 -0600

    DataStructures - Trees - Add NodeUtils

---
+++ DataStructures/Trees/Common/NodeUtils.java
@@ -0,0 +1,22 @@
+package DataStructures.Trees.Common;
+
+public class NodeUtils {
+    private NodeUtils() {
+    };
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+}
